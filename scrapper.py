ddcc823064c5187d3a1afb0f848434f0fa19bf2a
VictorHM <rcpoetics@gmail.com>
2019-12-18 19:24:15 +0100

    Cleaned comments.

--- scrapper.py
+++ scrapper.py
@@ -17,8 +17,6 @@ soup = bs(req.text, 'html.parser')
 
 #TODO generalize this to include any value inside class for links
 fichero = soup.find_all(class_="asociada-link ico-csv")
-#The following one is equivalent to previous
-#fichero2=soup(class_="asociada-link ico-csv")
 
 #Loop to iterate all items and download files with csv
 #HACK the href are incomplete, it needs before the link https://datos.madrid.es
@@ -26,26 +24,9 @@ links = []
 for i in fichero:
     links.append('https://datos.madrid.es'+i['href'])
 
-len(links)
-# To download csv files, that are test based, it is better to use wget
+# To download csv files, that are text based, it is better to use wget
 # Apparently requests lib downloads files in a binary format, and it is not
 # clear to me how to transform that to text.
 for elink in links:
     wget.download(elink, os.path.join('/home/victor/development/calidad_aire/', os.path.basename(elink)))
 
-# THIS SHOULD BE ALL
-
-#Now we have all in place to start downloading the files
-# for elink in links:
-#     res = requests.get(elink)
-#     try:
-#         res.raise_for_status()
-#     except Exception as exc:
-#         print('File ' + elink + ' failed with error %s' % (exc))
-# 
-#     #TODO save to a user defined path. for now is fixed
-#     dataFile = open(os.path.join('~/development/calidad_aire/', os.path.basename(elink)), 'wb')
-#     for chunk in res.iter_content(100000):
-#         dataFile.write(chunk)
-#     dataFile.close()
-#Finishedfor elink in 
